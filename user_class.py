bd5c2f10c8fd0039b27c72611f372af057728f77
jakob-nogler <jakob.nogler@gmail.com>
2018-06-27 19:36:44 +0200

    First implementation of User class Stores a limited amount of data of the user

---
+++ user_class.py
@@ -0,0 +1,62 @@
+from uuid import uuid4
+import hashlib
+from time import sleep
+
+
+class User:
+    """"
+    USER WILL BE A CLASS CONTAINING THE INFORMATION RELATED TO THE USER
+
+    CLASS USER
+    -self.name -> name of the user
+    -self.surname -> surname of the user
+    -self.mail -> mail of the user
+    -self.id -> id of user
+    -self.salt -> salt (is it more convenient to use rand instead of uuid4?)
+    -self.h_pw -> hashed password of the user
+    -self.organization -> Organization which the user belongs to
+
+    -METHODS:
+         1. get_name -> get the name of the user
+         2. get_surname -> get surname pf the user
+         3. get mail -> get mail of the user
+         4. get_id -> get id of the user
+         5. verify_pw -> method taking as argument a string verifying the validity of a password
+
+    -INITIALIZATION:
+         1. name -> name of the user
+         2. surname -> surname of the user
+         3. organization -> Organization which the user belongs to
+         4. mail -> mail of the user
+         5. pw -> password of the user
+    """
+    def __init__(self, name, surname, organization, mail, pw):
+        self.name = name
+        self.surname = surname
+        self.mail = mail
+        self.organization = organization
+        self.id = str(uuid4())
+        self.salt = str(uuid4().hex)
+        self.h_pw = hashlib.sha512((pw + self.salt).encode("utf-8")).hexdigest()
+
+    def __string__(self):
+        return self.name + " " + self.surname
+
+    def get_name(self):  # get the name of the user
+        return self.name
+
+    def get_surname(self):  # get mail of the user
+        return self.surname
+
+    def get_mail(self):  # get mail of the user
+        return self.mail
+
+    def get_id(self):  # get id of the user
+        return self.id
+
+    def verify_pw(self, pw):  # method taking as argument a string verifying the validity of a password
+        if hashlib.sha512((pw + self.salt).encode("utf-8")).hexdigest() == self.h_pw:
+            return True
+        else:
+            sleep(1)
+            return False
