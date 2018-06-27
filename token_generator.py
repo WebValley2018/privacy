79903bff53d3297517121aa9b32590c53e7498e1
jakob-nogler <jakob.nogler@gmail.com>
2018-06-27 18:27:49 +0200

    Updated the get_token function Now it returns a named tuple which represents the Token

--- token_generator.py
+++ token_generator.py
@@ -1,6 +1,9 @@
 from datetime import datetime, timedelta
 from uuid import uuid4
 import secrets
+from collections import namedtuple
+
+namedtuple('TokenTuple', 'token_value, act_time, dl_time, user')  # tuple for storing the tuple
 
 
 class Token:
@@ -19,13 +22,13 @@ class Token:
      2. get_dl_time -> get expiration date of the token
      3. get_rem_time -> get remaining time in which token will be valid
      4. get_token_value -> get value of the token
-     5. get_token -> get list of 4 elements which store self.token_value, self.act_time, self.dl_time, self.user
+     5. get_token -> get a tuple of 4 elements which stores self.token_value, self.act_time, self.dl_time, self.user
 
    -INITIALIZATION:
      1. token_lenght -> lenght of the token
      2. time delta -> time in which token will be valid
      3. user -> user id which created token
-     4. token_data -> array of 4 elements, if token_data is not None token_value, act_time, dl_time, user of the class will be get value of token_data
+     4. token_data -> tuple of 4 elements, if token_data is not None token_value, act_time, dl_time, user of the class will be get value of token_data
    """
     def __init__(self, token_length=16, time_delta=15, user=str(uuid4()), token_data=None):  # timedelta is in minutes
         if token_data is None:  # if token_data is not provided in arguments initialize with custom values
@@ -34,11 +37,11 @@ class Token:
             self.token_length = token_length  # length of the token
             self.token_value = secrets.token_hex(token_length)  # value of the token
             self.user = user  # id of the user which created the token
-        else:  # else initialize with values provided by the token_data list
-            self.token_value = token_data[0]
-            self.act_time = token_data[1]
-            self.dl_time = token_data[2]
-            self.user = token_data[3]
+        else:  # else initialize with values provided by the TokenTuple
+            self.token_value = token_data.token_value
+            self.act_time = token_data.act_time
+            self.dl_time = token_data.dl_time
+            self.user = token_data.user
             self.token_lenght = len(self.token_value)
 
     def __str__(self):
@@ -59,5 +62,9 @@ class Token:
     def get_token_value(self):  # get value of the token
         return self.token_value
 
-    def get_token(self):  # get list of 4 elements which store self.token_value, self.act_time, self.dl_time, self.user
-        return [self.token_value, self.act_time, self.dl_time, self.user]
+    def get_token(self):  # get named tuple which stores self.token_value, self.act_time, self.dl_time, self.user
+        tkn = namedtuple('TokenTuple', 'token_value, act_time, dl_time, user')  # named tuple "TokenTuple"
+        return tkn(self.token_value, self.act_time, self.dl_time, self.user)
+
+
+
