e6ebd9b28ccb4afdb8761eb798c7823ebc6a0e51
Eugenio Tampieri <eugenio.tampieri@icloud.com>
2018-06-28 15:14:22 +0200

    WIP webinterface

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
+class DB:
+    """Dummy DB class"""
+    def init(self):
+        pass
+    
+    def check_token(self, session_id):
+        return True
+    
+    def get_user_from_token(self, token_id):
+        user_id="qwertuiop"
+        return user_id
+
+
+database = DB()
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def mainPage():
+    if database.check_token(request.cookies.get("tovel_token")):
+        return "User page"
+    else:
+        return "Login page"
+
+@app.route("/logout")
+def logoutPage():
+    return "Reditrect to the /"
+
+@app.route("/login")
+def loginPage():
+    return "Check login and then redirect as needed"
 
 app.run(host='0.0.0.0')
