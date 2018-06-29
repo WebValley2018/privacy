7ca921f39acbd131c880a2bb013f62de704b4a88
jakob-nogler <jakob.nogler@gmail.com>
2018-06-29 22:15:03 +0200

    Updated db for admin class
    Signed-off-by: jakob-nogler <jakob.nogler@gmail.com>

--- admin.py
+++ admin.py
@@ -1,5 +1,6 @@
 from user import User
 from uuid import uuid4
+import pyotp
 import hashlib
 
 
@@ -18,10 +19,11 @@ class Admin(User):
         4. mail -> mail of the admin
 
     """
-    def __init__(self, username, name, surname, mail, pw):
+    def __init__(self, username, name, surname, pw):
         self.username = username
         self.name = name
         self.surname = surname
-        self.mail = mail
         self.salt = str(uuid4().hex)
+        self.id = 'A' + str(uuid4())
+        self.otp_key = pyotp.random_base32()
         self.h_pw = hashlib.sha512((pw + self.salt).encode("utf-8")).hexdigest()
--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from user import User
 from collections import namedtuple
 from auth_token import Token
 from time import localtime, time
-
+from admin import Admin
 
 class DB:
     """
@@ -13,10 +13,15 @@ class DB:
 
     -METHODS:
         1. get_user_from_id -> get user class from user id --> PROBLEM!! the created user class doesn't has pw
-        2. check_user -> check if user with given id exists
+        2. check_user -> check if user with given username exists
         3. check_token -> check if token with given value exists
-        4.  get_id_from_username -> get user id from username
+        4. get_id_from_username -> get user id from username
         5. register_token -> register new token in the db
+        6. check_mail -> check if user with given mail exists
+        7. set_token_ttl -> set ttl of a token to 0
+        8. register_user -> register new user in the db
+        9. register_admin -> register new admin in the db
+        10. check_admin -> check if admin with given username exists
 
     -INITIALIZATION:
         1. user -> user of db
@@ -35,12 +40,12 @@ class DB:
             new_user = User(user_data=user(i[0].decode('utf-8'), i[1].decode('utf-8'), i[2].decode('utf-8'), i[3].decode('utf-8'), i[4].decode('utf-8'), i[5], i[6].decode('utf-8'), i[7].decode('utf-8')))
             return new_user
 
-    def check_user(self, id):
+    '''def check_user(self, id):  # should check user get id or username???
         self.cursor.execute("SELECT count(1) FROM Users WHERE ID=%s", (id,))
         for i in self.cursor:
-            return True if i[0] == 1 else False
+            return True if i[0] == 1 else False'''
     
-    def check_token(self, token_value):
+    def check_token(self, token_value):  # check if token with given value exists
         if token_value is None:
             return False
         self.cursor.execute("SELECT count(1), TTL, CreationDate FROM Token WHERE TokenValue=%s", (token_value,))
@@ -50,26 +55,25 @@ class DB:
             else:
                 return False
 
-    def check_user(self, username):
+    def check_user(self, username):  # check if user with given username exists
         if username is None:
             return False
         self.cursor.execute("SELECT count(1) FROM Users WHERE Username=%s", (username,))
         for i in self.cursor:
             return True if i[0] else False
 
-    def check_mail(self, mail):
+    def check_mail(self, mail):  # check if user with given mail exists
         if mail is None:
             return False
         self.cursor.execute("SELECT count(1) FROM Users WHERE Mail=%s", (mail,))
         for i in self.cursor:
             return True if i[0] else False
 
-    def get_id_from_username(self, username):
+    def get_id_from_username(self, username):  # get user id from username
         if not self.check_user(username):
             return None
         self.cursor.execute("SELECT ID FROM Users WHERE Username=%s", (username,))
         for i in self.cursor:
-            print(i)
             return i[0].decode('utf-8')
         return None
     
@@ -77,14 +81,40 @@ class DB:
         self.cursor.execute("INSERT INTO Token VALUES (%s, %s, %s, %s);", (token.token_value, token.ttl, token.creation_date, token.user))
         self.mariadb_connection.commit()
 
-    def set_token_ttl(self, token_value):
+    def set_token_ttl(self, token_value):  # et ttl of a token to 0
         self.cursor.execute("UPDATE Token SET TTL = 0 WHERE TokenValue = %s", (token_value,))
 
-    def register_user(self, user):
+    def register_user(self, user):  # register new user in the db
         if self.check_user(user.username) or self.check_mail(user.mail):
             return False
         self.cursor.execute("INSERT INTO Users VALUES (%s, %s, %s, %s, %s, %s, %s, %s);", (user.username, user.name, user.surname, user.mail, user.id, user.salt, user.organization, user.trust))
         self.mariadb_connection.commit()
         return True
 
-    def
+    def check_admin(self, username):  # check if admin with given username exists
+        if username is None:
+            return False
+        self.cursor.execute("SELECT count(1) FROM Administrators WHERE Username=%s", (username,))
+        for i in self.cursor:
+            return True if i[0] else False
+
+    def register_admin(self, admin):  # register new admin in the db
+        if self.check_admin(admin.username):
+            return False
+        self.cursor.execute("INSERT INTO Administrators VALUES (%s, %s, %s, %s, %s, %s, %s);", (admin.id, admin.username, admin.name, admin.surname, admin.h_pw, admin.salt, admin.otp_key,))
+        self.mariadb_connection.commit()
+        return True
+
+    def prt(self):
+        self.cursor.execute("SELECT Name FROM Administrators")
+        for i in self.cursor:
+            print(i)
+
+
+db = DB()
+jakob = Admin('jiik', 'Jakob', 'Nogler', 'password')
+db.register_admin(jakob)
+db.prt()
+
+
+
