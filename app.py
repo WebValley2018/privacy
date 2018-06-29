24cc5bb104b2e7fea89d2563061e017ea178fe7f
jakob-nogler <jakob.nogler@gmail.com>
2018-06-29 12:08:48 +0200

    added logout and some other stuff
    Signed-off-by: jakob-nogler <jakob.nogler@gmail.com>

--- app.py
+++ app.py
@@ -10,35 +10,50 @@ ethereum=Ethereum()
 
 # Main page
 
+
 @app.route("/")
 def mainPage():
     if database.check_token(request.cookies.get("tovel_token")):
-        # If the user is logged in, let's display his 
+        # If the user is logged in, let's display his
         return "User page"
     else:
         with open("static-assets/login.html") as f:
-            if "loginfailed" in request.args:
-                return f.read().replace("{{loginmessage}}",'<div class="alert alert-danger" role="alert">Login failed. Please check your credentials</div>')
-            if "logoutsuccess" in request.args:
-                return f.read().replace("{{loginmessage}}",'<div class="alert alert-success" role="alert">Logout succeded</div>')
+            if "tovel_token" in request.cookies:
+                resp = make_response(redirect("/?sessionexpired"))
+                resp.set_cookie('tovel_token', '', expires=0)
+                return resp
+            elif "sessionexpired" in request.args:  # if redirected to "session expired" print Warning message
+                return f.read().replace("{{loginmessage}}", '''<div class="alert alert-warning" 
+                                            role="alert">Session expired or not valid</div>''')
+            elif "loginfailed" in request.args:    # if redirected to "loginfailed" print Error message
+                return f.read().replace("{{loginmessage}}", '''<div class="alert alert-danger"
+                                            role="alert">Login failed. Please check your credentials</div>''')
+            elif "logoutsuccess" in request.args:  # if redirected to "logoutsuccess" print Success message
+                return f.read().replace("{{loginmessage}}", '''<div class="alert alert-success"
+                                            role="alert">Logout succeded</div>''')
             else:
-                return f.read().replace("{{loginmessage}}",'')
+                return f.read().replace("{{loginmessage}}", '')
+
 
 @app.route("/logout")
 def logoutPage():
-    return "Redirect to the /"
+    resp = make_response(redirect("/?logoutsuccess"))
+    resp.set_cookie('tovel_token', '', expires=0)
+    return resp
+    #return "Redirect to the /"
+
 
 @app.route("/login", methods=["POST"])
 def loginPage():
     username = request.form["username"]
     password = request.form["password"]
-    user_id=database.get_id_from_username(username)
+    user_id = database.get_id_from_username(username)
     auth_id = ethereum.save_auth_attempt(user_id)
-    user=database.get_user_from_id(user_id)
+    user = database.get_user_from_id(user_id)
     user.set_pw_hash(ethereum.get_user(user_id).user_pwd_hash)
     if user.verify_pw(password):
         ethereum.save_auth_outcome(auth_id, True)
-        token=Token(user=user_id)
+        token = Token(user=user_id)
         database.register_token(token)
         resp = make_response(redirect("/"))
         resp.set_cookie("tovel_token", token.get_token_value())
@@ -48,4 +63,5 @@ def loginPage():
         resp = make_response(redirect("/?loginfailed"))
         return resp
 
-app.run(host='0.0.0.0', debug=True)
+
+app.run(host='0.0.0.0', debug=True)  # to do: remove the Bug True in production
