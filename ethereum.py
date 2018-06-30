a7bd0f82da43ef229e70561cb469920713d53432
Eugenio Tampieri <eugenio.tampieri@icloud.com>
2018-06-30 09:19:56 +0200

    Added comments

--- ethereum.py
+++ ethereum.py
@@ -14,38 +14,46 @@ namedtuple('userData', 'id, h_pw')  # user data (id and hashe pw
 
 class Ethereum:
     """
-    CLASS ETHEREUM FOR INTERACTING WITH THE BlOCK CHAIN
+    Ethereum class to deal with smart contracts
 
     -METHODS:
         1. get_user -> get namedtuple(id, and hashed pw) given id, as long there will ce no bc the string will be empty
-    -INITIALIZATION -> none
+        -- Jakob please implement
+    -INITIALIZATION -> FIXIT
     """
     def __init__(self, providerAddress = 'http://127.0.0.1:7545'):
+        #  Inizialize Web3 object
         self.w3 = Web3(Web3.HTTPProvider(providerAddress))
+        #  Now dealing with User details smart contract
         if not isfile("smart-contracts/user-details.json"):
             with open("smart-contracts/user-details.sol") as f:
+                #  Read Solidity source code
                 login_contract_source = f.read()  # read login smart contract from sourcefile
-            #with open("smart-contracts/auth-logging.sol") as f:
-            #    login_contract = f.read()
+            #  Compile Solidity
             compiled_login_contract = compile_source(login_contract_source)
+            #  Get interface for the contract
             login_contract_iface = compiled_login_contract["<stdin>:UserDetails"]
+            #  Start the registration contract onto the blockchain
             UserDetails = self.w3.eth.contract(abi=login_contract_iface['abi'], bytecode=login_contract_iface['bin'])
+            #  Tell the blockchain which account to use
             self.w3.eth.defaultAccount = self.w3.eth.accounts[0]
+            #  Initialize transaction
             tx_hash = UserDetails.constructor().transact()
+            #  Get transaction details
             tx_receipt = self.w3.eth.waitForTransactionReceipt(tx_hash)
+            #  Save ABI and contract address to file
             sc_ud_abi=login_contract_iface["abi"]
             sc_ud_address=tx_receipt.contractAddress
             f = open("smart-contracts/user-details.json", 'w')
             f.write(dumps({"address": sc_ud_address, "abi": sc_ud_abi}))
             f.close()
         else:
+            #  Load smart contract data from file
             with open("smart-contracts/user-details.json") as f:
                 dati_sc_ud = loads(f.read())
             sc_ud_address = dati_sc_ud["address"]
             sc_ud_abi = dati_sc_ud["abi"]
-        #print(dumps(UserDetails))
-        # Wait for the transaction to be mined, and get the transaction receipt
-        # Create the contract instance with the newly-deployed address
+        #  Prepare the smart contract
         self.user_details = self.w3.eth.contract(
             address=sc_ud_address,
             abi=sc_ud_abi,
